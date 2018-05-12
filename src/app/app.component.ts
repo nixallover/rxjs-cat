dc72b3b6d4e275b59a0dd775d2195510a7f0636f
nixallover <nicole.oliver.42@gmail.com>
2018-05-12 16:12:52 -0700

    created a basic interface that generates cats/marbles on button click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  marbles: any[] = [];
+
+  addMarble(): void {
+    this.marbles.push(1);
+  }
 }
